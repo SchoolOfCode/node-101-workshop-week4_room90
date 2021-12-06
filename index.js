485bcca16522e3a20749025f3c05883b9a7b72df
Marco Tsang <hkmarcoot@gmail.com>
2021-12-06 16:38:09 +0000

    Add Task 2d

--- index.js
+++ index.js
@@ -18,27 +18,46 @@ let myCollection = [
 
 // console.log(myCollection);
 
-function describeItem(item) {
+//Task 2c
+// function describeItem(item) {
+//   console.log(
+//     "I have a " +
+//       item[0].name +
+//       ". Here is what I like about it: " +
+//       item[0].whatILike
+//   );
+//   console.log(
+//     "I have " +
+//       item[1].count +
+//       " " +
+//       item[1].name +
+//       "s" +
+//       ". Here is what I like about them: " +
+//       item[1].whatILike
+//   );
+//   console.log(
+//     "I have a " +
+//       item[2].name +
+//       ". Here is what I like about it: " +
+//       item[2].whatILike
+//   );
+// }
+// describeItem(myCollection);
+
+//Task 2d
+function describeItem (item) {
   console.log(
     "I have a " +
-      item[0].name +
+      item.name +
       ". Here is what I like about it: " +
-      item[0].whatILike
-  );
-  console.log(
-    "I have " +
-      item[1].count +
-      " " +
-      item[1].name +
-      "s" +
-      ". Here is what I like about them: " +
-      item[1].whatILike
-  );
-  console.log(
-    "I have a " +
-      item[2].name +
-      ". Here is what I like about it: " +
-      item[2].whatILike
+      item.whatILike
   );
 }
-describeItem(myCollection);
+
+function describeCollection (items){
+  for (let i = 0; i < items.length; i++){
+    describeItem (items[i]);
+  }
+}
+
+console.log(describeCollection(myCollection));
